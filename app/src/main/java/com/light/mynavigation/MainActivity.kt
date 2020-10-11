cd9f11d46626ffe3dd3fe2b064a4b390fb2bde09
Alam <amalnuralam8@gmail.com>
2020-10-12 02:42:57 +0700

    finish create simple navigation

---
+++ app/src/main/java/com/light/mynavigation/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.light.mynavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
