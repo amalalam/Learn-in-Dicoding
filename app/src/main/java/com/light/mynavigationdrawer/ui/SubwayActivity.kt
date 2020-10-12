a33850ebe4f82c2e0374f4924fd61f7a2271c4c3
Alam <amalnuralam8@gmail.com>
2020-10-13 02:32:24 +0700

    Finish Create myNavigationDrawer (NavigationDrawerActivity)

---
+++ app/src/main/java/com/light/mynavigationdrawer/ui/SubwayActivity.kt
@@ -0,0 +1,12 @@
+package com.light.mynavigationdrawer.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.light.mynavigationdrawer.R
+
+class SubwayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_subway)
+    }
+}
